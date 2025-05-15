155caca8749517b0a72e33efe2ddc174a36d74cc
Juni Abdul Fajar <juni.fajar@Junis-MacBook-Air.local>
2025-05-15 15:50:09 +0700

    initial commit

---
+++ main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-east-1"
+  profile = "default"
+}
+
+resource "aws_s3_bucket" "test" {
+  tags = {
+    Name = "Test bucket"
+  }
+}
+resource "aws_instance" "app_server" {
+  ami           = "ami-0e58b56aa4d64231b" 
+  instance_type = "t2.micro"
+  tags = {
+    Name = "JunTestAwsInstance"
+  }
+}
+
---
+++ var.tf
@@ -0,0 +1,9 @@
+variable "ami_id" {
+  description = "AMI ID"
+  default = "ami-0aeade53a684c994b"
+}
+
+variable "subnet_prv1" {
+  description = "Private Subnet 1"
+  default = "subnet-0a8a965c050b6804b"
+}
